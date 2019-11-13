1a08a81d04f850dba754538b947a290dc0168b4a
util-you-com <util.you.com@gmail.com>
2019-11-13 21:19:08 +0800

    上传源码到dev分支

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -9,7 +9,7 @@ function integrateGitment(router) {
 
   router.afterEach((to) => {
     // 页面滚动，hash值变化，也会触发afterEach钩子，避免重新渲染
-    if (to.path === from.path) return
+    // if (to.path === from.path) return
     // 已被初始化则根据页面重新渲染 评论区
     if (scriptGitment.onload) {
       renderGitment()
@@ -57,3 +57,64 @@ export default ({
     console.error(e.message)
   }
 }
+
+
+
+
+
+// function integrateGitalk(router) {
+//   const linkGitalk = document.createElement('link');
+//   linkGitalk.href = 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.css';
+//   linkGitalk.rel = 'stylesheet';
+//   document.body.appendChild(linkGitalk);
+//   const scriptGitalk = document.createElement('script');
+//   scriptGitalk.src = 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js';
+//   document.body.appendChild(scriptGitalk);
+
+//   router.afterEach((to) => {
+//     if (scriptGitalk.onload) {
+//       loadGitalk(to);
+//     } else {
+//       scriptGitalk.onload = () => {
+//         loadGitalk(to);
+//       }
+//     }
+//   });
+
+//   function loadGitalk(to) {
+//     let commentsContainer = document.getElementById('gitalk-container');
+//     if (!commentsContainer) {
+//       commentsContainer = document.createElement('div');
+//       commentsContainer.id = 'gitalk-container';
+//       commentsContainer.classList.add('content');
+//     }
+//     const $page = document.querySelector('.page');
+//     if ($page) {
+//       $page.appendChild(commentsContainer);
+//       if (typeof Gitalk !== 'undefined' && Gitalk instanceof Function) {
+//         renderGitalk(to.fullPath);
+//       }
+//     }
+//   }
+//   function renderGitalk(fullPath) {
+//     const gitalk = new Gitalk({
+//       clientID: '103769ef64fae23f9964',	// 你的博客在github上的授权 clientId
+//       clientSecret: '00900497ad2cfe63044801825d3301b78241d7d3', // come from github development
+//       repo: 'blog-vuepress-talk',	// 你存储该博客评论的github新建的public类型的仓库
+//       owner: 'latinos-bub',
+//       admin: ['latinos-bub'],
+//       id: fullPath,	// 使用 fullPath 来给每个页面的评论单独设置
+//       distractionFreeMode: false,
+//       language: 'zh-CN',
+//     });
+//     gitalk.render('gitalk-container');
+//   }
+// }
+
+// export default ({Vue, options, router}) => {
+//   try {
+//     document && integrateGitalk(router)
+//   } catch (e) {
+//     console.error(e.message)
+//   }
+// }
